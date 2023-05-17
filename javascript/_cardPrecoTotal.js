3a6c4ba3c904027e0edcd3314f6f60dffe03c799
HugoFrajacomo <hugo.frajacomo@gmail.com>
2023-05-17 11:03:06 -0300

    Projeto Base
    Primeiro Commit

---
+++ javascript/_cardPrecoTotal.js
@@ -0,0 +1,13 @@
+function calcularValorTotal() {
+  // Obter o valor do campo de quantidade
+  var quantidade = document.getElementById("quantidadeProduto").value;
+
+  // Definir o preço unitário do produto (exemplo: R$ 10,00)
+  var precoUnitario = 2.75;
+
+  // Calcular o valor total
+  var valorTotal = quantidade * precoUnitario;
+
+  // Atualizar o campo de valor total com o resultado do cálculo
+  document.getElementById("valorTotal").value = valorTotal;
+}
